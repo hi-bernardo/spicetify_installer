3dd3825a4662c5264b269a39046e4a056b06b336
Hiago Bernardo <105991097+hi-bernardo@users.noreply.github.com>
2025-05-19 20:31:13 -0300

    Update ins_spotify.ps1

--- src/scripts/ins_spotify.ps1
+++ src/scripts/ins_spotify.ps1
@@ -1,29 +1,23 @@
-Write-Output "Baixando instalador offline do Spotify..."
-
 $spotifyUrl = "https://download.spotify.com/SpotifyFullSetup.exe"
 $downloadPath = "$env:TEMP\SpotifyFullSetup.exe"
 Invoke-WebRequest -Uri $spotifyUrl -OutFile $downloadPath
 
-Write-Output "Extraindo arquivos do Spotify..."
 $installPath = "$env:APPDATA\Spotify"
 Start-Process -Wait -FilePath $downloadPath -ArgumentList "/extract $installPath"
 
 # Criação do objeto COM
 $WScriptObj = New-Object -ComObject ("WScript.Shell")
 
-Write-Output "Criando atalho na área de trabalho..."
 $desktopShortcut = "$env:USERPROFILE\Desktop\Spotify.lnk"
 $shortcut = $WScriptObj.CreateShortcut($desktopShortcut)
 $shortcut.TargetPath = "$installPath\Spotify.exe"
 $shortcut.Save()
 
-Write-Output "Criando atalho no menu iniciar..."
 $startMenuShortcut = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Spotify.lnk"
 $shortcut = $WScriptObj.CreateShortcut($startMenuShortcut)
 $shortcut.TargetPath = "$installPath\Spotify.exe"
 $shortcut.Save()
 
-Write-Output "Registrando Spotify no Painel de Controle..."
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Spotify"
 New-Item -Path $regPath -Force | Out-Null
 Set-ItemProperty -Path $regPath -Name 'DisplayName' -Value 'Spotify'
@@ -36,7 +30,6 @@ Set-ItemProperty -Path $regPath -Name 'URLInfoAbout' -Value 'https://www.spotify
 Set-ItemProperty -Path $regPath -Name 'NoModify' -Value 1 -Type DWord
 Set-ItemProperty -Path $regPath -Name 'NoRepair' -Value 1 -Type DWord
 
-Write-Output "Fixando Spotify na barra de tarefas..."
 # Caminho para o atalho no menu iniciar (necessário para fixar)
 $shell = New-Object -ComObject Shell.Application
 $folder = $shell.Namespace((Split-Path $startMenuShortcut))
@@ -50,5 +43,3 @@ foreach ($verb in $verbs) {
         break
     }
 }
-
-Write-Output "Spotify instalado com sucesso!"
